263e8712ced5561450ecbc73c7265382e316eb2e
darivala <107341694+darivala@users.noreply.github.com>
2025-11-10 13:44:30 +0530

    Update variables.tf
    changing AMI and region

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region to launch the instance"
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
-  default     = "ami-01760eea5c574eb8" # Example This is our free ami in ap-south-1
+  default     = "ami-0157af9aea2eef346" # Example This is our free ami in ap-south-1
 }
 
 variable "instance_type" {
