757d9a44a410f23dde6e79cc60b7de7ab50b8ddd
darivala <107341694+darivala@users.noreply.github.com>
2025-11-11 19:50:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ provider "aws" {
 */
 # ----- S3 BUCKET -----
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345"
+  bucket = "my-unique-bucket-name-123456"
   acl    = "private"
 
   # Create this bucket only after EC2 instance is created
