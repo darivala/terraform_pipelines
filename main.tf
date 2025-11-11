c7805f072e04ae4cde29395edd968628aa4192ff
darivala <107341694+darivala@users.noreply.github.com>
2025-11-11 19:55:45 +0530

    Remove unused EC2 module and S3 bucket configuration
    Removed commented-out EC2 module and S3 bucket resource.

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 
-/*module "ec2_instance" {
+module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   name = "inst-crtd-terraform"
   ami = "ami-0157af9aea2eef346"
@@ -28,17 +28,5 @@ provider "aws" {
     Environment = "dev"
   }
 }
-*/
-# ----- S3 BUCKET -----
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-123456"
-  acl    = "private"
 
-  # Create this bucket only after EC2 instance is created
-  #depends_on = [module.ec2_instance]   # Wait until EC2 is ready
 
-  tags = {
-    Name        = "MyS3Bucket"
-    Environment = "Dev"
-  }
-}
